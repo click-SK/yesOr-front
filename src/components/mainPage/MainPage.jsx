17057c653dce4921416653ce8fc86ee4cc316578
click-SK <sashakushnir4991@gmail.com>
2023-06-18 21:11:52 +0300

    add header

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import '../../styles/mainPage.scss'
 
 const MainPage = () => {
     return (
-        <div>
+        <div className='banner_head'>
             <h1 style={{margin:'10px 0'}}>h1 : YOUR CHOICE IS TO DO GOOD OR...</h1>
             <h2 style={{margin:'10px 0'}}>h2 : About Us</h2>
             <p  style={{margin:'10px 0'}}> p : The Charitable Foundation is made up of dedicated agents and employees of Berkshire Hathaway HomeServices California Properties who want to make a positive and constructive impact on our local communities.</p>
