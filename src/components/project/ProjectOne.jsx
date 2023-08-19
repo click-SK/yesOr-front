e7d8b8763de2c8961db47fdeeb769f64d3b4096d
click-SK <sashakushnir4991@gmail.com>
2023-08-19 18:52:29 +0300

    add timer

--- src/components/project/ProjectOne.jsx
+++ src/components/project/ProjectOne.jsx
@@ -14,6 +14,10 @@ const ProjectOne = () => {
     const [donatsValue, setDonatsValue] = useState(0);
     const [fullName, setFullName] = useState('');
     const [timeLeft, setTimeLeft] = useState('');
+    const [deysLeft, setDaysLeft] = useState (null)
+    const [hourLeft, setHourLeft] = useState (null)
+    const [minutsLeft, setMinutsLeft] = useState (null)
+    const [secondLeft, setSecondLeft] = useState (null)
     const {user} = useSelector((state) => state.authUser.user);
     const [percentCollected, setPercentCollected] = useState(0);
     const [currentImg, setCurrentImg] = useState('/file/proj/1.png')
@@ -49,41 +53,41 @@ const ProjectOne = () => {
         }
     }, [currentProject]);
 
-    // useEffect(() => {
-    //     if(currentProject) {
-    //         function calculateTimeDifference(startDate, days) {
-    //             const currentDate = moment().utcOffset(3);
-    //             const futureDate = moment(startDate).add(days, 'days');
+    useEffect(() => {
+        if(currentProject) {
+            function calculateTimeDifference(startDate, days) {
+                const currentDate = moment().utcOffset(3);
+                const futureDate = moment(startDate).add(days, 'days');
               
-    //             const timeDifference = futureDate.diff(currentDate);
+                const timeDifference = futureDate.diff(currentDate);
               
-    //             const remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    //             const remainingHours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //             const remainingMinutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+                const remainingDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+                const remainingHours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const remainingMinutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
               
-    //             return {
-    //               days: remainingDays,
-    //               hours: remainingHours,
-    //               minutes: remainingMinutes
-    //             };
-    //           }
+                return {
+                  days: remainingDays,
+                  hours: remainingHours,
+                  minutes: remainingMinutes
+                };
+              }
 
-    //           console.log('date',moment().utcOffset(3)._d)
+              console.log('date',moment().utcOffset(3)._d)
               
-    //           // Приклад використання
-    //           const startDate = currentProject?.period?.startDate;
-    //           console.log('startDate',!!startDate);
-    //           const daysToAdd = currentProject?.period?.countDays;
+              // Приклад використання
+              const startDate = currentProject?.period?.startDate;
+              console.log('startDate',!!startDate);
+              const daysToAdd = currentProject?.period?.countDays;
               
-    //           const remainingTime = calculateTimeDifference(startDate, daysToAdd);
-    //           console.log('remainingTime',remainingTime.days);
-    //           if(!!startDate) {
-    //             setTimeLeft(`${remainingTime.days} days, ${remainingTime.hours} hours, ${remainingTime.minutes} minutes`)
-    //           } else {
-    //             setTimeLeft('0')
-    //           }
-    //     }
-    // },[currentProject])
+              const remainingTime = calculateTimeDifference(startDate, daysToAdd);
+              console.log('remainingTime',remainingTime.days);
+              if(!!startDate) {
+                setTimeLeft(`${remainingTime.days} days, ${remainingTime.hours} hours, ${remainingTime.minutes} minutes`)
+              } else {
+                setTimeLeft('0')
+              }
+        }
+    },[currentProject])
 
     const handleSavedProject = () => {
         axios.patch(`${BASE_URL}/saved-project`, {
@@ -121,37 +125,48 @@ const ProjectOne = () => {
         })
     }
 
-    console.log('currentProject', currentProject);
 
-    // const [timeLeft, setTimeLeft] = useState('');
-    // useEffect(() => {
-    //     if (currentProject && currentProject.period && currentProject.period.startDate && currentProject.period.countDays) {
-    //         const endDate = new Date(currentProject.period.startDate);
-    //         endDate.setDate(endDate.getDate() + currentProject.period.countDays);
 
-    //         const timerInterval = setInterval(() => {
-    //             const currentDate = new Date();
-    //             const timeDifference = endDate - currentDate;
+    
+    useEffect(() => {
+        if (currentProject && currentProject.period && currentProject.period.startDate && currentProject.period.countDays) {
+            const endDate = new Date(currentProject.period.startDate);
+            endDate.setDate(endDate.getDate() + currentProject.period.countDays);
+    
+            const timerInterval = setInterval(() => {
+                const currentDate = new Date();
+                const timeDifference = endDate - currentDate;
+    
+                if (timeDifference > 0) {
+                    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+                    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+                    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    
+                    const formattedDays = days.toString().padStart(2, '0');
+                    const formattedHours = hours.toString().padStart(2, '0');
+                    const formattedMinutes = minutes.toString().padStart(2, '0');
+                    const formattedSeconds = seconds.toString().padStart(2, '0');
+    
+                    setTimeLeft(`${formattedDays} : ${formattedHours} : ${formattedMinutes} : ${formattedSeconds}`);
+                    setDaysLeft(formattedDays);
+                    setHourLeft(formattedHours);
+                    setMinutsLeft(formattedMinutes);
+                    setSecondLeft(formattedSeconds);
+                } else {
+                    clearInterval(timerInterval);
+                    setTimeLeft('Time Expired');
+                }
+            }, 1000);
+    
+            return () => {
+                clearInterval(timerInterval);
+            };
+        }
+    }, [currentProject]);
 
-    //             if (timeDifference > 0) {
-    //                 const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    //                 const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //                 const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    //                 const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    //                 setTimeLeft(`${days} : ${hours} : ${minutes} : ${seconds}`);
-    //             } else {
-    //                 clearInterval(timerInterval);
-    //                 setTimeLeft('Time Expired');
-    //             }
-    //         }, 1000);
-
-    //         return () => {
-    //             clearInterval(timerInterval);
-    //         };
-    //     }
-    // }, [currentProject]);
-
+    console.log('currentProject', deysLeft);
 
     return (
     <div className='project_wraper'>
@@ -194,6 +209,24 @@ const ProjectOne = () => {
                         </div>
                     </div>
                     <div className='right_column'>
+                        <div className='timer_wraper'>
+                                <div className='timer_item'>
+                                    <p className='item_number'>{deysLeft} :</p>
+                                    <p className='item_text'>Days</p>
+                                </div>
+                                <div className='timer_item'>
+                                    <p className='item_number'>{hourLeft} :</p>
+                                    <p className='item_text'>Hours</p>
+                                </div>
+                                <div className='timer_item'>
+                                    <p className='item_number'>{minutsLeft} :</p>
+                                    <p className='item_text'>Minutes</p>
+                                </div>
+                                <div className='timer_item'>
+                                    <p className='item_number'>{secondLeft} </p>
+                                    <p className='item_text'>Second</p>
+                                </div>
+                        </div>
                         <div className='project_name_wrap'>
                             <img src='' alt="" />
                             <div>
