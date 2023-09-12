f4db18ae7775d9de57ee1b0f75d1eb4e2dc3c135
click-SK <sashakushnir4991@gmail.com>
2023-09-12 19:15:56 +0300

    change url

--- src/http/baseUrl.js
+++ src/http/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:4444/api';
-// export const BASE_URL = 'https://yesor-test-back-ff3915e00906.herokuapp.com/api';
+// export const BASE_URL = 'http://localhost:4444/api';
+export const BASE_URL = 'https://yesor-test-back-ff3915e00906.herokuapp.com/api';
