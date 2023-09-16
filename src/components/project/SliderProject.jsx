1ae7f59ddf4f4bab4de30335da4df37800b7b362
click-SK <sashakushnir4991@gmail.com>
2023-09-16 18:15:56 +0300

    fix video

--- src/components/project/SliderProject.jsx
+++ src/components/project/SliderProject.jsx
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { BsArrowDown, BsArrowUp } from 'react-icons/bs';
-import '../../styles/sliderProject.scss';
+import React, { useState, useEffect } from "react";
+import { BsArrowDown, BsArrowUp } from "react-icons/bs";
+import "../../styles/sliderProject.scss";
 
-const SliderProject = ({ currentExtension, BASE_URL, currentImg, imgProject, setCurrentImg }) => {
+const SliderProject = ({
+  currentExtension,
+  BASE_URL,
+  currentImg,
+  imgProject,
+  setCurrentImg,
+}) => {
   const [visibleImages, setVisibleImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp"];
+  const videoExtensions = [
+    "mp4",
+    "webm",
+    "ogg",
+    "avi",
+    "wmv",
+    "flv",
+    "mov",
+    "mkv",
+  ];
 
   useEffect(() => {
     if (imgProject.length > 0) {
@@ -48,24 +65,66 @@ const SliderProject = ({ currentExtension, BASE_URL, currentImg, imgProject, set
 
   return (
     <div className="project_description_wrap">
-      {currentExtension && ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp'].includes(currentExtension.toLowerCase())  && (
-        <img className="main_img-project_slide" src={`${BASE_URL}${currentImg}`} alt="" />
-      )}
-      {currentExtension && ['mp4', 'webm', 'ogg', 'avi', 'wmv', 'flv', 'mov', 'mkv'].includes(currentExtension.toLowerCase()) && (
-        <video controls className="main_img-project_slide" src={`${BASE_URL}${currentImg}`} alt="" />
-      )}
+      {currentExtension &&
+        ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp"].includes(
+          currentExtension.toLowerCase()
+        ) && (
+          <img
+            className="main_img-project_slide"
+            src={`${BASE_URL}${currentImg}`}
+            alt=""
+          />
+        )}
+      {currentExtension &&
+        ["mp4", "webm", "ogg", "avi", "wmv", "flv", "mov", "mkv"].includes(
+          currentExtension.toLowerCase()
+        ) && (
+          <video
+            controls
+            className="main_img-project_slide"
+            src={`${BASE_URL}${currentImg}`}
+            alt=""
+          />
+        )}
       <div className="second_img_wrap">
-        <div className="btn_wrap-slider btn-adaptive btn_up" onClick={handlePrevImage} onMouseDown={(e) => e.preventDefault()}>
+        <div
+          className="btn_wrap-slider btn-adaptive btn_up"
+          onClick={handlePrevImage}
+          onMouseDown={(e) => e.preventDefault()}
+        >
           <BsArrowUp />
         </div>
         <div className="second_img-project">
-          {visibleImages.map((data, idx) => (
-            <div key={idx} className="new_project_image_block">
-              <img key={idx} src={`${BASE_URL}${data}`} onClick={() => handleImageClick(data)} alt="" />
-            </div>
-          ))}
+          {visibleImages.map((data, idx) => {
+            const fileExtension = data.split(".").pop();
+            const fileType = imageExtensions.includes(
+              fileExtension.toLowerCase()
+            )
+              ? "image"
+              : videoExtensions.includes(fileExtension.toLowerCase())
+              ? "video"
+              : "unknown";
+
+            return (
+              <div key={idx} className="new_project_image_block">
+                {fileType === "image" ? (
+                  <img
+                    onClick={() => handleImageClick(data)}
+                    src={`${BASE_URL}${data}`}
+                    alt={`Image ${idx}`}
+                  />
+                ) : fileType === "video" ? (
+                  <video src={`${BASE_URL}${data}`} alt="" />
+                ) : null}
+              </div>
+            );
+          })}
         </div>
-        <div className="btn_wrap-slider btn-adaptive btn_down" onClick={handleNextImage} onMouseDown={(e) => e.preventDefault()}>
+        <div
+          className="btn_wrap-slider btn-adaptive btn_down"
+          onClick={handleNextImage}
+          onMouseDown={(e) => e.preventDefault()}
+        >
           <BsArrowDown />
         </div>
       </div>
