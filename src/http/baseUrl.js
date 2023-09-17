af96c00bcd3e27b09ee2cd5ceabe8b33371d4dff
click-SK <sashakushnir4991@gmail.com>
2023-09-17 21:42:23 +0300

    ch url

--- src/http/baseUrl.js
+++ src/http/baseUrl.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:4444/api';
-// export const BASE_URL = 'https://yes-or-server-9fb4dbc5fc9f.herokuapp.com/api';
-export const BASE_URL = 'https://yes-or-server-daf1a02044fe.herokuapp.com/api';
+export const BASE_URL = 'https://yes-or-server-9fb4dbc5fc9f.herokuapp.com/api';
+// export const BASE_URL = 'https://yes-or-server-daf1a02044fe.herokuapp.com/api';
